b4c170ce8c814f29f3a7577f4b21ff9abfba817f
adib-the-noob <mdadib550@gmail.com>
2024-09-16 02:07:40 +0600

    condiotional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-const LoginStatusFunction = (status) => {
-    if (status === true) {
-        return (
-            <div>
-                <h1>Welcome User</h1>
-                <button>Logout</button>
-            </div>
-        )
-    }
-    return (
-        <div>
-            <h1>Welcome Guest</h1>
-            <button>Login</button>
-        </div>
-    )
-}
 
 function App() {
-    return(<div>
-        {LoginStatusFunction(true)}
-    </div>)
+    const user_status = false;
+    return (
+        <div>
+            {(()=>{
+                if(user_status){
+                    return (
+                        <div>
+                            <h1>Welcome User</h1>
+                            <button>Logout</button>
+                        </div>
+                    )
+                }else{
+                    return (
+                        <div>
+                            <h1>Welcome Guest</h1>
+                            <button>Login</button>
+                        </div>
+                    )
+                }
+            })()}
+        </div>
+    )
 }
 
 export default App
