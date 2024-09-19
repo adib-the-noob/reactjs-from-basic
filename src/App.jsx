0e8fa217de3483107b0e73447a6d3ddd3b399a6a
adib-the-noob <mdadib550@gmail.com>
2024-09-20 02:36:45 +0600

    todo maded

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
 import { useState } from "react";
 
 const App = () => {
-    const [ myObj, setMyObj] = useState({
-        "userName" : "ADIB",
-        "passWord" : "hacker"
-    });
+    const [list, setList] = useState([]);
+    const [item, setItem] = useState("");
 
-    const changeValueObj = () => {
-        setMyObj(
-            prebObj => ({
-                ...prebObj,
-                "userName" : "Abdul Adib"
-            })
-        )
+    const addItem = () => {
+        list.push(item);
+        setList([...list]);
+    };
+
+    const removeItem = (index) => {
+        list.splice(index, 1);
+        setList([...list]);
     }
+
     return (
         <div>
-            <p>This users name is {myObj.userName}</p>
-        <button onClick={changeValueObj}>ChangeName</button>
+            <table>
+                <tbody>
+                    {
+                        (list.length !== 0) ? list.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item}</td>
+                                <td><button onClick={() => {
+                                    removeItem(index);
+                                }}>Remove</button></td>
+                            </tr>
+                        )) : <tr><td>No items</td></tr>
+                    }
+                </tbody>
+            </table>
+            <h1>{list.length}</h1>
+            <input onChange={(e) => setItem(e.target.value)} placeholder="item"/>
+            <button onClick={addItem}>Add</button>
         </div>
     )
-}
+};
 
 export default App;
